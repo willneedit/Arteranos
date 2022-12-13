f0be317e7b8d38a174c5bef7c5e4539b33992c16
willneedit <willneedit@nurfuerspam.de>
2022-12-13 19:42:20 +0100

    Runtime switching between VR and desktop

--- Assets/Network/AvatarManager.cs
+++ Assets/Network/AvatarManager.cs
@@ -1,6 +1,8 @@
 
+using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace NetworkIO
 {
@@ -25,9 +27,20 @@ namespace NetworkIO
 
         static void StartButtons()
         {
-            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
-            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+            if (GUILayout.Button("Host"))
+            {
+                NetworkManager.Singleton.StartHost();
+            }
+            
+            if (GUILayout.Button("Client")) 
+            {
+                NetworkManager.Singleton.StartClient();
+            }
+
+            if (GUILayout.Button("Server")) 
+            {
+                NetworkManager.Singleton.StartServer();
+            }
         }
 
         static void StatusLabels()
---
+++ Assets/Scripts/XRControl.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.XR.Management;
+
+public class XRControl : MonoBehaviour
+{
+    public bool enableVR = false;
+    private bool currentVR = false;
+
+    public IEnumerator StartXRCoroutine()
+    {
+        Debug.Log("Initializing XR...");
+        yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
+
+        if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+        {
+            Debug.LogError("Initializing XR Failed. Check Editor or Player log for details.");
+        }
+        else
+        {
+            Debug.Log("Starting XR...");
+            XRGeneralSettings.Instance.Manager.StartSubsystems();
+        }
+    }
+
+    void StopXR()
+    {
+        Debug.Log("Stopping XR...");
+
+        XRGeneralSettings.Instance.Manager.StopSubsystems();
+        XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+        Debug.Log("XR stopped completely.");
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enableVR == currentVR) return;
+
+        currentVR = enableVR;
+
+        if(enableVR)
+            StartCoroutine(StartXRCoroutine());
+        else
+            StopXR();
+    }
+
+    // Cleanly shut down XR on exit.
+    void OnApplicationQuit()
+    {
+        // Debug.Log("Bye!");
+        if(enableVR)
+            StopXR();
+    }
+
+}
