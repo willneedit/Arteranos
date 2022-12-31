4d963e2734f8d6d3e001b6819b88e94b61daa8c5
willneedit <willneedit@nurfuerspam.de>
2022-12-31 14:30:28 +0100

    Cleanup

--- Assets/Network/AvatarManager.cs
+++ Assets/Network/AvatarManager.cs
@@ -18,8 +18,6 @@ namespace NetworkIO
             else
             {
                 StatusLabels();
-
-                // SubmitNewPosition();
             }
 
             GUILayout.EndArea();
@@ -53,22 +51,5 @@ namespace NetworkIO
             GUILayout.Label("Mode: " + mode);
         }
 
-        static void SubmitNewPosition()
-        {
-            if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change"))
-            {
-                if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient)
-                {
-                    foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
-                        NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<AvatarPlayer>().Move();
-                }
-                else
-                {
-                    var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
-                    var player = playerObject.GetComponent<AvatarPlayer>();
-                    player.Move();
-                }
-            }
-        }
     }
 }
--- Assets/Network/AvatarPlayer.cs
+++ Assets/Network/AvatarPlayer.cs
@@ -19,37 +19,11 @@ namespace NetworkIO
             }
         }
 
-        public void Move()
-        {
-            if (NetworkManager.Singleton.IsServer)
-            {
-                var randomPosition = GetRandomPositionOnPlane();
-                transform.position = randomPosition;
-                Position.Value = randomPosition;
-            }
-            else
-            {
-                SubmitPositionRequestServerRpc();
-            }
-        }
-
-        [ServerRpc]
-        public void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
-        {
-            // Position.Value = transform.position;
-            Position.Value = GetRandomPositionOnPlane();
-        }
-
         [ServerRpc]
         public void UpdateCurrentPositionServerRpc(Vector3 position, Quaternion rotation, ServerRpcParams rpcParams = default)
         {
             Position.Value = position;
             Rotation.Value = rotation;
-        }
-
-        static Vector3 GetRandomPositionOnPlane()
-        {
-            return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
         }
 
         void Update()
