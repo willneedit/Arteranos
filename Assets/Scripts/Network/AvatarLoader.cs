7ee2b2ee37f2cae64035568dfd9d3927959d403b
willneedit <willneedit@nurfuerspam.de>
2023-01-06 20:31:19 +0100

    Frame for the Avatar loading

--- Assets/Scripts/Network/AvatarLoader.cs
+++ Assets/Scripts/Network/AvatarLoader.cs
@@ -10,42 +10,48 @@ namespace NetworkIO
     public class AvatarLoader : NetworkBehaviour
     {
 
-        public NetworkVariable<FixedString512Bytes> m_AvatarURL;
+        public NetworkVariable<FixedString512Bytes> m_AvatarURL = new NetworkVariable<FixedString512Bytes>(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+        public GameObject m_AvatarStandin = null;
         public GameObject m_AvatarObject = null;
         private bool loading = false;
 
+        private GameObject m_SpawnedStandin = null;
+
         public override void OnNetworkSpawn()
         {
-            m_AvatarURL.Value = "https://api.readyplayer.me/v1/avatars/6394c1e69ef842b3a5112221.glb";
+            m_AvatarURL.OnValueChanged += OnAvatarURLChanged;
+
+            // DEBUG
+            if(IsOwner)
+                m_AvatarURL.Value = "https://api.readyplayer.me/v1/avatars/6394c1e69ef842b3a5112221.glb";
+
+            m_SpawnedStandin = Instantiate(m_AvatarStandin, transform.position, transform.rotation);
+            m_SpawnedStandin.transform.SetParent(transform);
         }
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            
-        }
-
-        // Update is called once per frame
-        void Update()
+        void OnAvatarURLChanged(FixedString512Bytes old, FixedString512Bytes current)
         {
             if(loading) return;
 
-            if(!m_AvatarURL.Value.IsEmpty)
-            {
-                loading = true;
-                StartCoroutine(ProcessLoader());
-            }
+            loading = true;
+            StartCoroutine(ProcessLoader(current));
         }
 
-        IEnumerator ProcessLoader()
+        IEnumerator ProcessLoader(FixedString512Bytes current)
         {
-            Debug.Log("Starting avatar loading: " + m_AvatarURL.Value);
+            Debug.Log("Starting avatar loading: " + current);
             yield return new WaitForSeconds(5);
 
-            GameObject newAv = Instantiate(m_AvatarObject, transform.position, transform.rotation);
-            newAv.GetComponent<NetworkObject>().SpawnWithOwnership(OwnerClientId);
+            if(m_SpawnedStandin != null)
+            {
+                Destroy(m_SpawnedStandin);
+                m_SpawnedStandin = null;
+            }
 
-            this.GetComponent<NetworkObject>().Despawn();
+            GameObject newAv = Instantiate(m_AvatarObject, transform.position, transform.rotation);
+            newAv.transform.SetParent(transform);
+            loading = false;
         }
+
     }
 }
