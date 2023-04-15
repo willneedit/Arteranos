585063791fe0e7d1228d896917fd1e11112fa62b
willneedit <willneedit@nurfuerspam.de>
2023-04-15 10:39:13 +0200

    Moved Authorization base to the web assembly

--- Assets/Arteranos/Scripts/Core/Types.cs
+++ Assets/Arteranos/Scripts/Core/Types.cs
@@ -11,13 +11,6 @@ using System;
 using Mirror;
 using System.ComponentModel;
 using System.Reflection;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-using Cdm.Authentication.Browser;
-using Cdm.Authentication.OAuth2;
-using Cdm.Authentication.Clients;
-using System.Linq;
 
 namespace Arteranos.ExtensionMethods
 {
@@ -117,36 +110,4 @@ namespace Arteranos.Core
 
 namespace Arteranos.Auth
 {
-    public interface ILoginPackage
-    {
-        public AuthorizationCodeFlow GetAuthorizationCodeFlow();
-        public Task<(string, string)> GetUserIDAsync(AuthenticationSession session);
-    }
-
-    public class MockServerPackage : ILoginPackage
-    {
-        public AuthorizationCodeFlow GetAuthorizationCodeFlow()
-        {
-            AuthorizationCodeFlow.Configuration configMock = new()
-            {
-                clientId = "my_client_id",
-                clientSecret = "my_client_secret",
-#if UNITY_IOS && !UNITY_EDITOR
-                redirectUri = "com.cdm.myauthapp:/oauth2",
-#else
-                redirectUri = "http://localhost:8080/myauthapp/oauth2/",
-#endif
-                scope = "openid email profile"
-            };
-            return new MockServerAuth(configMock, "http://localhost:8001");
-        }
-        public async Task<(string, string)> GetUserIDAsync(AuthenticationSession session) => await Task.Run(() => ("MockServerUser", "MSU#0000"));
-    }
-
-    public struct PackageListEntry
-    {
-        public string name;
-        public ILoginPackage pack;
-    }
-
 }
---
+++ Assets/Arteranos/Scripts/Web/AuthorizationRoot.cs
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2023, willneedit
+ * 
+ * Licensed by the Mozilla Public License 2.0,
+ * residing in the LICENSE.md file in the project's root directory.
+ */
+
+using Cdm.Authentication.Clients;
+using Cdm.Authentication.OAuth2;
+using System.Threading.Tasks;
+
+namespace Arteranos.Auth
+{
+    public interface ILoginPackage
+    {
+        public AuthorizationCodeFlow GetAuthorizationCodeFlow();
+        public Task<(string, string)> GetUserIDAsync(AuthenticationSession session);
+    }
+
+    public class MockServerPackage : ILoginPackage
+    {
+        public AuthorizationCodeFlow GetAuthorizationCodeFlow()
+        {
+            AuthorizationCodeFlow.Configuration configMock = new()
+            {
+                clientId = "my_client_id",
+                clientSecret = "my_client_secret",
+#if UNITY_IOS && !UNITY_EDITOR
+                redirectUri = "com.cdm.myauthapp:/oauth2",
+#else
+                redirectUri = "http://localhost:8080/myauthapp/oauth2/",
+#endif
+                scope = "openid email profile"
+            };
+            return new MockServerAuth(configMock, "http://localhost:8001");
+        }
+        public async Task<(string, string)> GetUserIDAsync(AuthenticationSession session) => await Task.Run(() => ("MockServerUser", "MSU#0000"));
+    }
+
+    public struct PackageListEntry
+    {
+        public string name;
+        public ILoginPackage pack;
+    }
+}
