b92dbd63b432238265e10380a93c18c8301e2578
willneedit <willneedit@nurfuerspam.de>
2023-04-21 16:47:14 +0200

    Fixed missing namespace filing.

--- Assets/Arteranos/Scripts/UI/Windows/DialogUI.cs
+++ Assets/Arteranos/Scripts/UI/Windows/DialogUI.cs
@@ -7,89 +7,92 @@ using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DialogUI : UIBehaviour
+namespace Arteranos.UI
 {
-    public string text = "Dialog text";
-    public string[] buttons = null;
-
-    public TMP_Text Caption = null;
-    public GameObject ButtonPane = null;
-
-    public event Action<int> OnDialogDone;
-
-    private readonly SemaphoreSlim dialogFinished = new(0, 1);
-
-    public static DialogUI New()
+    public class DialogUI : UIBehaviour
     {
-        GameObject go = Instantiate(Resources.Load("UI/UI_Dialog") as GameObject);
-        return go.GetComponent<DialogUI>();
-    }
+        public string text = "Dialog text";
+        public string[] buttons = null;
 
-    protected override void Start()
-    {
-        UnityAction makeButtonPressedAction(int index) => () => OnButtonClicked(index);
-        GameObject sampleButton = ButtonPane.transform.GetChild(0).gameObject;
-        GameObject sampleSpacer = ButtonPane.transform.GetChild(1).gameObject;
+        public TMP_Text Caption = null;
+        public GameObject ButtonPane = null;
 
-        base.Start();
+        public event Action<int> OnDialogDone;
 
-        Caption.text = text;
+        private readonly SemaphoreSlim dialogFinished = new(0, 1);
 
-        // No buttons, only to close by outside means.
-        if(buttons == null)
+        public static DialogUI New()
         {
-            Destroy(sampleButton);
-            Destroy(sampleSpacer);
-            return;
+            GameObject go = Instantiate(Resources.Load("UI/UI_Dialog") as GameObject);
+            return go.GetComponent<DialogUI>();
         }
 
-        if(buttons.Length != 0)
+        protected override void Start()
         {
-            sampleButton.GetComponent<Button>().onClick.AddListener(makeButtonPressedAction(0));
-            sampleButton.GetComponentInChildren<TMP_Text>().text = buttons[0];
+            UnityAction makeButtonPressedAction(int index) => () => OnButtonClicked(index);
+            GameObject sampleButton = ButtonPane.transform.GetChild(0).gameObject;
+            GameObject sampleSpacer = ButtonPane.transform.GetChild(1).gameObject;
+
+            base.Start();
+
+            Caption.text = text;
+
+            // No buttons, only to close by outside means.
+            if(buttons == null)
+            {
+                Destroy(sampleButton);
+                Destroy(sampleSpacer);
+                return;
+            }
+
+            if(buttons.Length != 0)
+            {
+                sampleButton.GetComponent<Button>().onClick.AddListener(makeButtonPressedAction(0));
+                sampleButton.GetComponentInChildren<TMP_Text>().text = buttons[0];
+            }
+            else
+                Destroy(sampleButton);
+
+
+            if(buttons.Length < 2)
+                Destroy(sampleSpacer);
+
+            for(int i = 1; i < buttons.Length; i++)
+            {
+                if(i > 1)
+                    Instantiate(sampleSpacer, ButtonPane.transform);
+
+                Button btn = Instantiate(sampleButton, ButtonPane.transform).GetComponent<Button>();
+                btn.onClick.AddListener(makeButtonPressedAction(i));
+                btn.GetComponentInChildren<TMP_Text>().text = buttons[i];
+            }
+
+
+
         }
-        else
-            Destroy(sampleButton);
 
-
-        if(buttons.Length < 2)
-            Destroy(sampleSpacer);
-
-        for(int i = 1; i < buttons.Length; i++)
+        private void OnButtonClicked(int index)
         {
-            if(i > 1)
-                Instantiate(sampleSpacer, ButtonPane.transform);
-
-            Button btn = Instantiate(sampleButton, ButtonPane.transform).GetComponent<Button>();
-            btn.onClick.AddListener(makeButtonPressedAction(i));
-            btn.GetComponentInChildren<TMP_Text>().text = buttons[i];
+            OnDialogDone?.Invoke(index);
+            dialogFinished.Release();
         }
 
+        // Purely convenient for write a process driven control flow rather than
+        // a event driven one and reducing the boilerplate.
+        public async Task<int> PerformDialogAsync(string text, string[] buttons)
+        {
+            this.text = text;
+            this.buttons = buttons;
 
+            int rc = -1;
 
-    }
+            OnDialogDone += (index) => rc = index;
+            await dialogFinished.WaitAsync();
+            OnDialogDone -= (index) => rc = index;
 
-    private void OnButtonClicked(int index)
-    {
-        OnDialogDone?.Invoke(index);
-        dialogFinished.Release();
-    }
+            Destroy(gameObject);
 
-    // Purely convenient for write a process driven control flow rather than
-    // a event driven one and reducing the boilerplate.
-    public async Task<int> PerformDialogAsync(string text, string[] buttons)
-    {
-        this.text = text;
-        this.buttons = buttons;
-
-        int rc = -1;
-
-        OnDialogDone += (index) => rc = index;
-        await dialogFinished.WaitAsync();
-        OnDialogDone -= (index) => rc = index;
-
-        Destroy(gameObject);
-
-        return rc;
+            return rc;
+        }
     }
 }
