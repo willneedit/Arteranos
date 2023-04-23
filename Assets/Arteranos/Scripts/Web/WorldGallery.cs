910390fe1ed92dcdf75bf86c7a00f98c24c71ba9
willneedit <willneedit@nurfuerspam.de>
2023-04-23 11:57:01 +0200

    Framework for the world gallery and its list item.

---
+++ Assets/Arteranos/Scripts/Web/WorldGallery.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Copyright (c) 2023, willneedit
+ * 
+ * Licensed by the Mozilla Public License 2.0,
+ * residing in the LICENSE.md file in the project's root directory.
+ */
+
+using System.IO;
+using UnityEngine;
+
+namespace Arteranos.Web
+{
+    public class WorldGallery
+    {
+        private static string GetRootPath(string url, bool cached)
+        {
+            return cached
+                ? $"{Application.temporaryCachePath}/WorldCache/{WorldDownloader.GetURLHash(url)}/world.dir"
+                : $"{Application.persistentDataPath}/WorldGallery/{WorldDownloader.GetURLHash(url)}";
+        }
+
+        public static (string, string) RetrieveWorld(string url, bool cached = false)
+        {
+            string rootPath = GetRootPath(url, cached);
+
+            if(!Directory.Exists(rootPath)) return (null, null);
+
+            string metadataFile = $"{rootPath}/Metadata.json";
+
+            if(!File.Exists(metadataFile)) metadataFile = null;
+
+            string screenshotFile = null;
+            foreach(string file in Directory.EnumerateFiles(rootPath, "Screenshot.*"))
+            {
+                screenshotFile = file;
+                break;
+            }    
+
+            return (metadataFile, screenshotFile);
+        }
+
+        public static bool StoreWorld(string url)
+        {
+            string metadataFile;
+            string screenshotFile;
+
+            (metadataFile, screenshotFile) = RetrieveWorld(url, true);
+
+            // Nothing at all?
+            if(string.IsNullOrEmpty(metadataFile) && string.IsNullOrEmpty(screenshotFile)) return false;
+
+            string rootPath = GetRootPath(url, false);
+
+            Directory.CreateDirectory(rootPath);
+
+            if(metadataFile != null)
+                File.Copy(metadataFile, $"{rootPath}/Metadata.json");
+
+            if(screenshotFile != null)
+                File.Copy(screenshotFile, $"{rootPath}/{Path.GetFileName(screenshotFile)}");
+
+            return true;
+        }
+
+        public static void DeleteWorld(string url)
+        {
+            string rootPath = GetRootPath(url, false);
+
+            if(Directory.Exists(rootPath)) Directory.Delete(rootPath);
+        }
+    }
+}
