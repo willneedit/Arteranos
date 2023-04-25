6fa14b3b51fd8cc6b0d8b4c5b56c57bfcf1774ec
willneedit <willneedit@nurfuerspam.de>
2023-04-25 16:22:48 +0200

    Created ListItemBase for future Server, Avatar and User lists

---
+++ Assets/Arteranos/Scripts/UI/Components/ListItemBase.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Copyright (c) 2023, willneedit
+ * 
+ * Licensed by the Mozilla Public License 2.0,
+ * residing in the LICENSE.md file in the project's root directory.
+ */
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Arteranos.UI
+{
+    public class ListItemBase : UIBehaviour
+    {
+        public HoverButton btn_Add = null;
+
+        public HoverButton btn_Background = null;
+        public HoverButton btn_Delete = null;
+        public HoverButton btn_Visit = null;
+        public GameObject go_Overlay = null;
+
+        private bool ChildControlEntered = false;
+
+        Coroutine delayco = null;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            btn_Background.onHover += OnShowControls;
+            btn_Background.interactable = false;
+
+            btn_Add.onHover += OnShowChildControls;
+            btn_Visit.onHover += OnShowChildControls;
+            btn_Delete.onHover += OnShowChildControls;
+        }
+
+        private IEnumerator DelayShowOverlay(bool entered)
+        {
+            delayco = null;
+
+            yield return new WaitForSeconds(1);
+
+            if(ChildControlEntered || entered)
+                entered = true;
+
+            go_Overlay.SetActive(entered);
+        }
+
+        private void OnShowChildControls(bool entered) => ChildControlEntered = entered;
+
+        private void OnShowControls(bool entered)
+        {
+            if(ChildControlEntered) return;
+
+            if(delayco != null) StopCoroutine(delayco);
+
+            delayco = StartCoroutine(DelayShowOverlay(entered));
+        }
+    }
+}
--- Assets/Arteranos/Scripts/UI/Components/WorldListItem.cs
+++ Assets/Arteranos/Scripts/UI/Components/WorldListItem.cs
@@ -7,10 +7,8 @@
 
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 using Arteranos.Web;
@@ -20,19 +18,11 @@ using Arteranos.Core;
 
 namespace Arteranos.UI
 {
-    public class WorldListItem : UIBehaviour
+    public class WorldListItem : ListItemBase
     {
         public string worldURL = null;
-
-        public HoverButton btn_Background = null;
         public Image img_Screenshot = null;
         public TMP_Text lbl_Caption = null;
-        public GameObject go_Overlay = null;
-        public HoverButton btn_Add = null;
-        public HoverButton btn_Visit = null;
-        public HoverButton btn_Delete = null;
-
-        private bool ChildControlEntered = false;
 
         public static WorldListItem New(Transform parent, string url)
         {
@@ -47,43 +37,11 @@ namespace Arteranos.UI
         {
             base.Awake();
 
-            btn_Background.onHover += OnShowControls;
-            btn_Background.interactable = false;
-
-            btn_Add.onHover += OnShowChildControls;
-            btn_Visit.onHover += OnShowChildControls;
-            btn_Delete.onHover += OnShowChildControls;
-
             btn_Add.onClick.AddListener(OnAddClicked);
             btn_Visit.onClick.AddListener(OnVisitClicked);
             btn_Delete.onClick.AddListener(OnDeleteClicked);
 
             go_Overlay.SetActive(false);
-        }
-
-        private void OnShowChildControls(bool entered) => ChildControlEntered = entered;
-
-        Coroutine delayco = null;
-
-        private void OnShowControls(bool entered)
-        {
-            if(ChildControlEntered) return;
-
-            if(delayco != null) StopCoroutine(delayco);
-
-            delayco = StartCoroutine(DelayShowOverlay(entered));
-        }
-
-        private IEnumerator DelayShowOverlay(bool entered)
-        {
-            delayco = null;
-
-            yield return new WaitForSeconds(1);
-
-            if(ChildControlEntered || entered)
-                entered = true;
-
-            go_Overlay.SetActive(entered);
         }
 
         protected override void Start()
