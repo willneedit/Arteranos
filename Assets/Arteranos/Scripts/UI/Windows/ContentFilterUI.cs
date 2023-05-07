0e1dc8bcc1448e3a481e72493252e2056fabe086
willneedit <willneedit@nurfuerspam.de>
2023-05-07 11:38:52 +0200

    Made Content Filter UI dynamic to do away with the hardcoded UI layout

--- Assets/Arteranos/Scripts/UI/Windows/ContentFilterUI.cs
+++ Assets/Arteranos/Scripts/UI/Windows/ContentFilterUI.cs
@@ -6,7 +6,6 @@
  */
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -16,10 +15,28 @@ using UnityEngine.UI;
 using Arteranos.Core;
 using UnityEngine.Events;
 
+using System.Reflection;
+
 namespace Arteranos.UI
 {
     public class ContentFilterUI : UIBehaviour
     {
+        internal struct ContentFilterEntry
+        {
+            public string Name;
+            public string FieldName;
+            public string Description;
+
+            public ContentFilterEntry(string _Name, string _FieldName, string _Description)
+            {
+                Name = _Name;
+                FieldName = _FieldName;
+                Description = _Description;
+            }
+        };
+
+        public event Action OnFinishConfiguring;
+
         public Transform tbl_Table;
         public TextMeshProUGUI lbl_HelpText;
 
@@ -28,6 +45,41 @@ namespace Arteranos.UI
         private readonly List<Button> btns_Help = new();
         private readonly List<Spinner> spns_Config = new();
 
+        internal readonly ContentFilterEntry[] Filters = new ContentFilterEntry[]
+        {
+            new("Nudity",
+                "Nudity",
+                "<b>Nudity</b>\n" +
+                    "Nudity, non-sexual or artistic."
+                ),
+            new("Suggestive",
+                "Suggestive",
+                "<b>(sexually) suggestive</b>\n" +
+                    "Not including nudity, but characters depicted in the content look like " +
+                    "or behave with the flirtatious actions or in an outright " +
+                    "sexual manner."
+                ),
+            new("Violence",
+                "Violence",
+                "<b>Cartoon / \"Clean\" violence</b>\n" +
+                    "Violence, but no depictions of lasting harm, no blood, no gore."
+                ),
+            new("Explicit Nudity",
+                "ExplicitNudes",
+                "<b>Explicit Nudity</b>\n" +
+                    "Detailed depiction of sexual acts or derivation thereof, " +
+                    "be it still or animated.\n" +
+                    "<color=#ff8888><b>Not for children at all!</b></color>"
+                ),
+            new("Excessive Violence",
+                "ExcessiveViolence",
+                "<b>Realistic/Excessive violence</b>\n" +
+                    "1. Violence, depicted in a realistic manner. Blood and/or gore.\n" +
+                    "2. Excessive violence. Self-harm, or torture.\n" +
+                    "<color=#ff8888><b>Not for children at all!</b></color>"
+                )
+        };
+    
         public static ContentFilterUI New()
         {
             GameObject go = Instantiate(Resources.Load<GameObject>("UI/UI_ContentFilter"));
@@ -46,67 +98,42 @@ namespace Arteranos.UI
             base.Awake();
 
             UnityAction makeShowHelpText(int row) =>
-                () => ShowHelpText(row);
+                () => lbl_HelpText.text = Filters[row].Description;
 
             Action<int, bool> makeOnSpinnerClicked(int row) =>
-                (val, up) => OnSpinnerClicked(row, val);
+                (val, up) => lbl_HelpText.text = string.Empty;
 
+            // Take the first (and only) row as a blueprint for the remaining rows
+            Transform DescColumn = tbl_Table.GetChild(0);
             Transform HelpColumn = tbl_Table.GetChild(1);
             Transform ConfigColumn = tbl_Table.GetChild(2);
 
-            Debug.Assert(HelpColumn.childCount == ConfigColumn.childCount);
+            TextMeshProUGUI lbl_tmpl = DescColumn.GetChild(0).GetComponent<TextMeshProUGUI>();
+            Button btn_tmpl = HelpColumn.GetChild(0).GetComponent<Button>();
+            Spinner spn_tmpl = ConfigColumn.GetChild(0).GetComponent<Spinner>();
 
-            for(int i = 0, c = ConfigColumn.childCount;i < c;i++)
+            for(int i = 0, c = Filters.Length;i < c;i++)
             {
-                Button btn = HelpColumn.GetChild(i).GetComponent<Button>();
-                btn.onClick.AddListener(makeShowHelpText(i));
-                btns_Help.Add(btn);
+                TextMeshProUGUI lbl = lbl_tmpl;
+                Button btn = btn_tmpl;
+                Spinner spn = spn_tmpl;
 
-                Spinner spn = ConfigColumn.GetChild(i).GetComponent<Spinner>();
+                if(i > 0)
+                {
+                    lbl = Instantiate(lbl_tmpl, DescColumn, false);
+                    btn = Instantiate(btn_tmpl, HelpColumn, false);
+                    spn = Instantiate(spn_tmpl, ConfigColumn, false);
+                }
+
+                lbl.text = Filters[i].Name;
+
+                btn.onClick.AddListener(makeShowHelpText(i));
+
                 spn.OnChanged += makeOnSpinnerClicked(i);
                 spn.Options = spnOptions;
-                spns_Config.Add(spn);
             }
 
             lbl_HelpText.text = string.Empty;
-        }
-
-        private void OnSpinnerClicked(int _1, int _2) => lbl_HelpText.text = string.Empty;
-
-        private void ShowHelpText(int row)
-        {
-            string txt = null;
-            switch(row)
-            {
-                case 0: txt =
-                        "<b>Explicit Nudity</b>\n" +
-                        "Detailed depiction of sexual acts or derivation thereof, " +
-                        "be it still or animated.\n" +
-                        "<color=#ff8888><b>Not for children at all!</b></color>";
-                    break;
-                case 1: txt =
-                        "<b>Nudity</b>\n" +
-                        "Nudity, non-sexual or artistic.\n";
-                    break;
-                case 2: txt =
-                        "<b>(sexually) suggestive</b>\n" +
-                        "Not including nudity, but characters depicted in the content look like " +
-                        "or behave with the flirtatious actions or in an outright " +
-                        "sexual manner.";
-                    break;
-                case 3: txt =
-                        "<b>Cartoon / \"Clean\" violence</b>\n" +
-                        "Violence, but no depictions of lasting harm, no blood, no gore.";
-                    break;
-                case 4: txt =
-                        "<b>Realistic/Excessive violence</b>\n" +
-                        "1. Violence, depicted in a realistic manner. Blood and/or gore.\n" +
-                        "2. Excessive violence. Self-harm, or torture.\n" +
-                        "<color=#ff8888><b>Not for children at all!</b></color>";
-                    break;
-            }
-
-            lbl_HelpText.text = txt;
         }
 
         bool? Spn2bool(Spinner spn)
@@ -129,19 +156,24 @@ namespace Arteranos.UI
             };
         }
 
-        // I _could_ use Reflections to indirectly access fields of an object,
-        // but it's too much boilerplate for just five fields.
+        private readonly Dictionary<string, FieldInfo> spjfields = new();
+
         protected override void Start()
         {
             base.Start();
 
             Debug.Assert(spj != null, "No association of the Content Filter UI");
 
-            spns_Config[0].value = Bool2spn(spj.ExplicitNudes);
-            spns_Config[1].value = Bool2spn(spj.Nudity);
-            spns_Config[2].value = Bool2spn(spj.Suggestive);
-            spns_Config[3].value = Bool2spn(spj.Violence);
-            spns_Config[4].value = Bool2spn(spj.ExcessiveViolence);
+            FieldInfo[] fields = spj.GetType().GetFields();
+            foreach(FieldInfo field in fields) spjfields[field.Name] = field;
+
+            Transform ConfigColumn = tbl_Table.GetChild(2);
+
+            for(int i = 0, c = Filters.Length; i < c; ++i)
+            {
+                Spinner spn = ConfigColumn.GetChild(i).GetComponent<Spinner>();
+                spn.value = Bool2spn(spjfields[Filters[i].FieldName].GetValue(spj) as bool?);
+            }
         }
 
         protected override void OnDisable()
@@ -150,11 +182,15 @@ namespace Arteranos.UI
 
             Debug.Assert(spj != null, "No association of the Content Filter UI");
 
-            spj.ExplicitNudes = Spn2bool(spns_Config[0]);
-            spj.Nudity = Spn2bool(spns_Config[1]);
-            spj.Suggestive = Spn2bool(spns_Config[2]);
-            spj.Violence = Spn2bool(spns_Config[3]);
-            spj.ExcessiveViolence = Spn2bool(spns_Config[4]);
+            Transform ConfigColumn = tbl_Table.GetChild(2);
+
+            for(int i = 0, c = Filters.Length; i < c; ++i)
+            {
+                Spinner spn = ConfigColumn.GetChild(i).GetComponent<Spinner>();
+                spjfields[Filters[i].FieldName].SetValue(spj, Spn2bool(spn));
+            }
+
+            OnFinishConfiguring?.Invoke();
         }
     }
 }
