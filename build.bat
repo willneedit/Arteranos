3bec8e3770318742ea32f27d3572e66eb28597e7
willneedit <willneedit@nurfuerspam.de>
2023-05-20 12:07:41 +0200

    Fixed the installer executable to customize the installation location

--- build.bat
+++ build.bat
@@ -5,18 +5,18 @@ set unity="D:\Unity_Editors\2021.3.15f1\Editor\Unity.exe"
 
 rmdir /s /q build
 
-rmdir /s /q Library\ScriptAssemblies
-rmdir /s /q Library\BurstCache
+:: rmdir /s /q Library\ScriptAssemblies
+:: rmdir /s /q Library\BurstCache
 
 %unity% -quit -batchmode -projectpath %projectpath% -executeMethod Arteranos.BuildPlayers.BuildWin64DedServ -logFile build\build64DedServ.log
 
-rmdir /s /q Library\ScriptAssemblies
-rmdir /s /q Library\BurstCache
+:: rmdir /s /q Library\ScriptAssemblies
+:: rmdir /s /q Library\BurstCache
 
 %unity% -quit -batchmode -projectpath %projectpath% -executeMethod Arteranos.BuildPlayers.BuildWin64 -logFile build\build64.log
 
-rmdir /s /q Library\ScriptAssemblies
-rmdir /s /q Library\BurstCache
+:: rmdir /s /q Library\ScriptAssemblies
+:: rmdir /s /q Library\BurstCache
 
 cd build
 
@@ -31,13 +31,13 @@ move Win64\Arteranos.exe .
 
 "%wix%"\bin\candle ..\Setup\Main.wxs Win64-Server.wxi Win64.wxi -dBinDir=Win64 -dSrvBinDir=Win64-Server -arch x64
 
-"%wix%"\bin\light Main.wixobj Win64.wixobj Win64-Server.wixobj -ext WixUIExtension -o Arteranos
+"%wix%"\bin\light Main.wixobj Win64.wixobj Win64-Server.wixobj -ext WixUIExtension -o ArteranosSetup
 
 move Arteranos.exe Win64
 
 "%wix%"\bin\candle.exe -ext WixNetFxExtension -ext WixBalExtension -ext WixUtilExtension ..\Setup\MainBurn.wxs
 
-"%wix%"\bin\light.exe -ext WixNetFxExtension -ext WixBalExtension -ext WixUtilExtension MainBurn.wixobj -o Arteranos
+"%wix%"\bin\light.exe -ext WixNetFxExtension -ext WixBalExtension -ext WixUtilExtension MainBurn.wixobj -o ArteranosSetup
 
 
 cd ..
