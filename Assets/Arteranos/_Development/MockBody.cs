46a9b43be42b0cce2df382b9ca2c53798826e1d0
willneedit <willneedit@nurfuerspam.de>
2023-05-28 18:26:59 +0200

    Added some development-only code

---
+++ Assets/Arteranos/_Development/MockBody.cs
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2023, willneedit
+ * 
+ * Licensed by the Mozilla Public License 2.0,
+ * residing in the LICENSE.md file in the project's root directory.
+ */
+
+#if UNITY_EDITOR
+
+using Arteranos.Avatar;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arteranos
+{
+    public class MockBody : MonoBehaviour, IAvatarLoader
+    {
+        public Transform LeftHand => throw new System.NotImplementedException();
+
+        public Transform RightHand => throw new System.NotImplementedException();
+
+        public Transform LeftFoot => throw new System.NotImplementedException();
+
+        public Transform RightFoot => throw new System.NotImplementedException();
+
+        public Quaternion LhrOffset => throw new System.NotImplementedException();
+
+        public Quaternion RhrOffset => throw new System.NotImplementedException();
+
+        public Transform CenterEye => throw new System.NotImplementedException();
+
+        public Transform Head => throw new System.NotImplementedException();
+
+        public float FootElevation => throw new System.NotImplementedException();
+
+        public string GalleryModeURL { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public float EyeHeight => 1.85f;
+
+        public float FullHeight => 1.75f;
+
+        public void ResetPose() => throw new System.NotImplementedException();
+        public void UpdateOpenMouth(float amount) => throw new System.NotImplementedException();
+    }
+}
+
+#endif
---
+++ Assets/Arteranos/_Development/MockBrain.cs
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2023, willneedit
+ * 
+ * Licensed by the Mozilla Public License 2.0,
+ * residing in the LICENSE.md file in the project's root directory.
+ */
+
+#if UNITY_EDITOR
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arteranos.Avatar
+{
+    public class MockBrain : MonoBehaviour, IAvatarBrain
+    {
+        public string AvatarURL => "6394c1e69ef842b3a5112221";
+
+        public int ChatOwnID => 9999;
+
+        public uint NetID => 9999;
+
+        public byte[] UserHash => new byte[0];
+
+        public string Nickname => "TI-99 4a";
+
+        public int NetMuteStatus { get => 0; set { } }
+
+        public bool IsOwned => false;
+
+        public bool ClientMuted { get => m_ClientMuted; set => m_ClientMuted = value; }
+
+        public IAvatarLoader Body => GetComponent<IAvatarLoader>();
+
+        public event Action<string> OnAvatarChanged;
+        public event Action<int> OnNetMuteStatusChanged;
+
+        public void OnDestroy() { }
+        public void OnStartClient() { }
+        public void OnStartServer() { }
+        public void OnStopClient() { }
+
+
+        private bool m_ClientMuted = false;
+
+        private void Start()
+        {
+            if(!IsOwned)
+                AvatarHitBoxFactory.New(gameObject);
+        }
+
+    }
+}
+
+#endif
