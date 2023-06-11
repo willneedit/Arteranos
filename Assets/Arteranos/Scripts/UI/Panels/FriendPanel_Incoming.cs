5134e01111ba72020493194f93df8f76454de6d1
willneedit <willneedit@nurfuerspam.de>
2023-06-11 10:45:38 +0200

    see #17

---
+++ Assets/Arteranos/Scripts/UI/Panels/FriendPanel_Incoming.cs
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023, willneedit
+ * 
+ * Licensed by the Mozilla Public License 2.0,
+ * residing in the LICENSE.md file in the project's root directory.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Arteranos.Core;
+using Arteranos.Avatar;
+using Arteranos.Social;
+using Arteranos.XR;
+
+namespace Arteranos.UI
+{
+    public class FriendPanel_Incoming : FriendPanelBase
+    {
+        public override IEnumerable<SocialListEntryJSON> GetSocialListTab()
+        {
+            // FIXME Collection was modified
+            IEnumerable<SocialListEntryJSON> list = cs.GetSocialList(null, IsFriendRequested);
+            foreach(SocialListEntryJSON entry in list) yield return entry;
+        }
+
+        private bool IsFriendRequested(SocialListEntryJSON arg)
+        {
+            if((arg.state & SocialState.Friend_offered) != SocialState.Friend_offered) return false;
+
+            IAvatarBrain targetUser = SettingsManager.GetOnlineUser(arg.UserID);
+
+            if(targetUser == null)
+                // Last I've seen....
+                return (arg.state & SocialState.Friend_bonded) == SocialState.Friend_bonded;
+            else
+                // Or, just in case, update the status
+                return XRControl.Me.IsMutualFriends(targetUser);
+        }
+    }
+}
