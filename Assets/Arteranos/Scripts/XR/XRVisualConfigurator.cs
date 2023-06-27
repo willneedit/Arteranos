7e47f4f440b7a854f94b29ed6c565e5708608566
willneedit <willneedit@nurfuerspam.de>
2023-06-27 11:05:25 +0200

    Implemented comfort blinders, see #25

--- Assets/Arteranos/Scripts/XR/XRVisualConfigurator.cs
+++ Assets/Arteranos/Scripts/XR/XRVisualConfigurator.cs
@@ -6,29 +6,53 @@
  */
 
 using Arteranos.Core;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+
 
 namespace Arteranos.XR
 {
     public class XRVisualConfigurator : MonoBehaviour
     {
+        // The required speed (1m/s) to fully close the shutters (or rather to fully use the effects)
+        [SerializeField] private float VelocityMax = 1.0f;
+
+        // 1/4th of a second to close/open the shutters
+        [SerializeField] private float TweenSpeed = 4.0f;
+
         private Vector3 pos = Vector3.zero;
+        private float BlindersMaxValue = 0.0f;
+
+        private float TweenedD = 0.0f;
+
+        private Volume vol;
 
         void Start()
         {
+            vol = GetComponentInChildren<Volume>();
+
             SettingsManager.Client.OnXRControllerChanged += DownloadControlSettings;
             DownloadControlSettings();
         }
 
         void Update()
         {
-            Transform t = XRControl.Me?.gameObject.transform;
+            float d;
+        
+            Transform t = Camera.main != null ? Camera.main.transform : null;
 
             if(t == null) return;
 
-            float d = Vector3.SqrMagnitude(t.position - pos);
+            d = Vector3.Magnitude(t.position - pos) / Time.deltaTime;
+            d = Mathf.Clamp01(d / VelocityMax);
+            pos = t.position;
+
+            if(d > TweenedD) TweenedD += TweenSpeed * Time.deltaTime;
+            if(d < TweenedD) TweenedD -= TweenSpeed * Time.deltaTime;
+            TweenedD = Mathf.Clamp01(TweenedD);
+
+            // Too much hassle to pick out the vignette. Just use the whole global volume.
+            if(vol != null) vol.weight = TweenedD * BlindersMaxValue;
         }
 
         private void OnDestroy()
@@ -42,6 +66,21 @@ namespace Arteranos.XR
 
             if(mcs == null) return;
 
+            switch(mcs.ComfortBlinders)
+            {
+                case ComfortBlindersType.Off:
+                    BlindersMaxValue = 0.0f;
+                    break;
+                case ComfortBlindersType.Low:
+                    BlindersMaxValue = 0.33f;
+                    break;
+                case ComfortBlindersType.Medium:
+                    BlindersMaxValue = 0.66f;
+                    break;
+                case ComfortBlindersType.High:
+                    BlindersMaxValue = 1.0f;
+                    break;
+            }
         }
     }
 }
