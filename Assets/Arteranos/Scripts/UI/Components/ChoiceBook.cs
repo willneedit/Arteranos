ce18e4a141377349dcb8db3ff7e3aaf7d7c14c9f
willneedit <willneedit@nurfuerspam.de>
2024-07-29 11:09:13 +0200

    Chore: Fixed some namespace spillage

--- Assets/Arteranos/Scripts/UI/Components/ChoiceBook.cs
+++ Assets/Arteranos/Scripts/UI/Components/ChoiceBook.cs
@@ -5,73 +5,76 @@ using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ChoiceBook : UIBehaviour
+namespace Arteranos.UI
 {
-    [Serializable]
-    public struct ChoiceBookEntry
+    public class ChoiceBook : UIBehaviour
     {
-        public string name;
-        public UIBehaviour UI;
-    }
-
-    public ChoiceBookEntry[] ChoiceEntries = null;
-    public int CurrentChoice = 0;
-
-    public event Action<int, int> OnChoicePageChanged;
-
-    public Transform ButtonList { get; private set; } = null;
-    public Transform PaneList { get; private set; } = null;
-
-    protected override void Awake()
-    {
-        UnityAction makeButtonPressedAction(int index) => () => OnButtonClicked(index);
-
-        base.Start();
-
-        Debug.Assert(ChoiceEntries != null && ChoiceEntries.Length > 0,
-            "Choicebook has at least one page");
-
-        ButtonList = transform.GetChild(0);
-        PaneList = transform.GetChild(1);
-
-        Button SampleButton = ButtonList.GetChild(0).gameObject.GetComponent<Button>();
-
-        SampleButton.GetComponent<Button>().onClick.AddListener(makeButtonPressedAction(0));
-        SampleButton.GetComponentInChildren<TMP_Text>().text = ChoiceEntries[0].name;
-
-        for(int i = 1; i < ChoiceEntries.Length; i++)
+        [Serializable]
+        public struct ChoiceBookEntry
         {
-            Button btn = Instantiate(SampleButton, ButtonList);
-            btn.onClick.AddListener(makeButtonPressedAction(i));
-            btn.GetComponentInChildren<TMP_Text>().text = ChoiceEntries[i].name;
+            public string name;
+            public UIBehaviour UI;
         }
 
-        for(int j = 0; j < ChoiceEntries.Length; j++)
+        public ChoiceBookEntry[] ChoiceEntries = null;
+        public int CurrentChoice = 0;
+
+        public event Action<int, int> OnChoicePageChanged;
+
+        public Transform ButtonList { get; private set; } = null;
+        public Transform PaneList { get; private set; } = null;
+
+        protected override void Awake()
         {
-            ChoiceEntries[j].UI.gameObject.SetActive(false);
-            GameObject go = Instantiate(ChoiceEntries[j].UI.gameObject, PaneList);
+            UnityAction makeButtonPressedAction(int index) => () => OnButtonClicked(index);
+
+            base.Start();
+
+            Debug.Assert(ChoiceEntries != null && ChoiceEntries.Length > 0,
+                "Choicebook has at least one page");
+
+            ButtonList = transform.GetChild(0);
+            PaneList = transform.GetChild(1);
+
+            Button SampleButton = ButtonList.GetChild(0).gameObject.GetComponent<Button>();
+
+            SampleButton.GetComponent<Button>().onClick.AddListener(makeButtonPressedAction(0));
+            SampleButton.GetComponentInChildren<TMP_Text>().text = ChoiceEntries[0].name;
+
+            for (int i = 1; i < ChoiceEntries.Length; i++)
+            {
+                Button btn = Instantiate(SampleButton, ButtonList);
+                btn.onClick.AddListener(makeButtonPressedAction(i));
+                btn.GetComponentInChildren<TMP_Text>().text = ChoiceEntries[i].name;
+            }
+
+            for (int j = 0; j < ChoiceEntries.Length; j++)
+            {
+                ChoiceEntries[j].UI.gameObject.SetActive(false);
+                GameObject go = Instantiate(ChoiceEntries[j].UI.gameObject, PaneList);
+            }
+
+            PaneList.GetChild(CurrentChoice).gameObject.SetActive(true);
         }
 
-        PaneList.GetChild(CurrentChoice).gameObject.SetActive(true);
-    }
+        private void OnButtonClicked(int newChoice)
+        {
+            if (CurrentChoice == newChoice) return;
 
-    private void OnButtonClicked(int newChoice)
-    {
-        if(CurrentChoice == newChoice) return;
+            PaneList.GetChild(CurrentChoice).gameObject.SetActive(false);
+            PaneList.GetChild(newChoice).gameObject.SetActive(true);
 
-        PaneList.GetChild(CurrentChoice).gameObject.SetActive(false);
-        PaneList.GetChild(newChoice).gameObject.SetActive(true);
+            OnChoicePageChanged?.Invoke(CurrentChoice, newChoice);
+            CurrentChoice = newChoice;
+        }
 
-        OnChoicePageChanged?.Invoke(CurrentChoice, newChoice);
-        CurrentChoice = newChoice;
-    }
+        public void SetPageActive(int index, bool active)
+        {
+            PaneList.GetChild(index).gameObject.SetActive(active);
+            ButtonList.GetChild(index).gameObject.SetActive(active);
 
-    public void SetPageActive(int index, bool active)
-    {
-        PaneList.GetChild(index).gameObject.SetActive(active);
-        ButtonList.GetChild(index).gameObject.SetActive(active);
-
-        // Pulling the rug from under your feet?
-        if (!active && CurrentChoice == index && CurrentChoice > 0) OnButtonClicked(CurrentChoice - 1);
+            // Pulling the rug from under your feet?
+            if (!active && CurrentChoice == index && CurrentChoice > 0) OnButtonClicked(CurrentChoice - 1);
+        }
     }
 }
--- Assets/Arteranos/Scripts/UI/Support/CameraUITracker.cs
+++ Assets/Arteranos/Scripts/UI/Support/CameraUITracker.cs
@@ -5,74 +5,76 @@
  * residing in the LICENSE.md file in the project's root directory.
  */
 
-using Arteranos;
 using UnityEngine;
 
-public class CameraUITracker : MonoBehaviour
+namespace Arteranos.UI
 {
-    public float m_Delay = 2.0f;
-    public float m_Duration = 5.0f;
-    public float m_Tolerance = 1.00f;
-    public Vector3 m_offset = Vector3.forward;
-    public Quaternion m_rotation = Quaternion.identity;
-
-    private float m_countdown;
-    private GameObject m_camera;
-    private float m_currentSpeed = 0.0f;
-    private bool m_moving = false;
-
-    private bool initial = true;
-    private Collider Collider = null;
-
-    // Update is called once per frame
-    void LateUpdate()
+    public class CameraUITracker : MonoBehaviour
     {
-        m_camera = Camera.main != null ? Camera.main.gameObject : null;
-        if(m_camera == null) return;
+        public float m_Delay = 2.0f;
+        public float m_Duration = 5.0f;
+        public float m_Tolerance = 1.00f;
+        public Vector3 m_offset = Vector3.forward;
+        public Quaternion m_rotation = Quaternion.identity;
 
-        float vrfactor = (G.Client?.VRMode == true) ? 2.0f : 1.0f;
-        Vector3 relOffset = m_camera.transform.rotation * (m_offset * vrfactor);
+        private float m_countdown;
+        private GameObject m_camera;
+        private float m_currentSpeed = 0.0f;
+        private bool m_moving = false;
 
-        if(initial)
+        private bool initial = true;
+        private Collider Collider = null;
+
+        // Update is called once per frame
+        void LateUpdate()
         {
-            Collider = gameObject.GetComponentInChildren<Collider>();
+            m_camera = Camera.main != null ? Camera.main.gameObject : null;
+            if (m_camera == null) return;
 
-            if(Collider != null) Collider.enabled = false;
+            float vrfactor = (G.Client?.VRMode == true) ? 2.0f : 1.0f;
+            Vector3 relOffset = m_camera.transform.rotation * (m_offset * vrfactor);
 
-            // VR guideline: approach the viewer from the front, a bit to the side.
-            Vector3 iniOffset = m_camera.transform.rotation * new Vector3(5, 0, 10);
-            transform.SetPositionAndRotation(m_camera.transform.position + iniOffset, m_camera.transform.rotation);
-            initial = false;
-            return;
+            if (initial)
+            {
+                Collider = gameObject.GetComponentInChildren<Collider>();
+
+                if (Collider != null) Collider.enabled = false;
+
+                // VR guideline: approach the viewer from the front, a bit to the side.
+                Vector3 iniOffset = m_camera.transform.rotation * new Vector3(5, 0, 10);
+                transform.SetPositionAndRotation(m_camera.transform.position + iniOffset, m_camera.transform.rotation);
+                initial = false;
+                return;
+            }
+
+            float tolerance = m_moving ? 0.001f : m_Tolerance;
+            float dist = Vector3.Distance(transform.position - relOffset, m_camera.transform.position);
+
+            if (dist < tolerance)
+            {
+                m_countdown = m_Delay;
+                m_currentSpeed = 0.0f;
+                m_moving = false;
+
+                if (Collider != null) Collider.enabled = true;
+                return;
+            }
+
+            m_countdown -= Time.deltaTime;
+
+            if (m_countdown > 0) return;
+
+            m_moving = true;
+            if (Collider != null) Collider.enabled = false;
+
+            if (m_currentSpeed < tolerance) m_currentSpeed = tolerance;
+
+            Vector3 target = m_camera.transform.position + relOffset;
+            Vector3 destination = Vector3.Lerp(transform.position, target, -m_countdown / m_Duration);
+            Quaternion destrot = Quaternion.Lerp(transform.rotation, m_camera.transform.rotation * m_rotation, -m_countdown / m_Duration);
+
+            transform.SetPositionAndRotation(destination, destrot);
+
         }
-
-        float tolerance = m_moving ? 0.001f : m_Tolerance;
-        float dist = Vector3.Distance(transform.position - relOffset, m_camera.transform.position);
-
-        if(dist < tolerance)
-        {
-            m_countdown = m_Delay;
-            m_currentSpeed = 0.0f;
-            m_moving = false;
-
-            if(Collider != null) Collider.enabled = true;
-            return;
-        }
-
-        m_countdown -= Time.deltaTime;
-
-        if(m_countdown > 0) return;
-
-        m_moving = true;
-        if(Collider != null) Collider.enabled = false;
-
-        if(m_currentSpeed < tolerance) m_currentSpeed = tolerance;
-
-        Vector3 target = m_camera.transform.position + relOffset;
-        Vector3 destination = Vector3.Lerp(transform.position, target, -m_countdown / m_Duration);
-        Quaternion destrot = Quaternion.Lerp(transform.rotation, m_camera.transform.rotation * m_rotation, -m_countdown / m_Duration);
-
-        transform.SetPositionAndRotation(destination, destrot);
-
     }
 }
