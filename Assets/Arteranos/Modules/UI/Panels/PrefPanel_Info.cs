f3419f20d5db35e52a2def93390e798d69ba9de6
willneedit <willneedit@nurfuerspam.de>
2025-02-09 10:43:11 +0100

    fix: Fixed file header comment because of overzealous search&replace

--- Assets/Arteranos/Modules/UI/Panels/PrefPanel_Info.cs
+++ Assets/Arteranos/Modules/UI/Panels/PrefPanel_Info.cs
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2023, willneedit
  * 
- * Licensed by the Mozilla [SerializeField] private License 2.0,
+ * Licensed by the Mozilla Public License 2.0,
  * residing in the LICENSE.md file in the project's root directory.
  */
 
